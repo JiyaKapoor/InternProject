95fe6216e508cb78ac31495a45de757f97dcdaa8
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-10 22:45:01 +0530

    Email to be parsed to give a Ticket Object

--- demo/src/main/java/com/InternProject/demo/Config/KafkaTopicConfig.java
+++ demo/src/main/java/com/InternProject/demo/Config/KafkaTopicConfig.java
@@ -7,7 +7,6 @@ import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
-
     @Bean
     public NewTopic ticketsRawTopic() {
         return TopicBuilder.name("tickets.raw")
