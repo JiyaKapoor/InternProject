539c86a801e58fbe4b23fea8d4ccf978b4ab84f4
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-17 16:53:10 +0530

    Ticket repository Jpa Interface corrected

--- demo/src/main/java/com/InternProject/demo/Repository/TicketRepository.java
+++ demo/src/main/java/com/InternProject/demo/Repository/TicketRepository.java
@@ -3,5 +3,5 @@ package com.InternProject.demo.Repository;
 import com.InternProject.demo.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TicketRepository extends JpaRepository<String, Ticket>{
+public interface TicketRepository extends JpaRepository<Ticket,String>{
 }
